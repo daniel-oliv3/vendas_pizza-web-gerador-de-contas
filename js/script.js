bdce7678da2821949efa375ca554d9a4809f2abd
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-02 13:01:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,8 @@ btnFinalizarPedido.addEventListener('click', FinalizarPedido, false);
 
 
 //Funções
+
+//Função vendas
 function venda(e){
     if(document.getElementById('mussarela').checked){
         somaMussarela = somaMussarela + 1;
@@ -39,7 +41,37 @@ function venda(e){
     }
 }
 
-
+//Função Estorno
+function estorno(e){
+    if(document.getElementById('mussarela').checked){
+        somaMussarela = somaMussarela - 1;
+        vendaMussarela = vendaMussarela - 25.00;
+        if(somaMussarela < 0){
+            somaMussarela = 0;
+            vendaMussarela = 0.00;
+        }
+        document.getElementById('resultadoMussarela').innerHTML = somaMussarela;
+        document.getElementById('vendaMussarela').innerHTML = vendaMussarela.toFixed(2);
+    }else if(document.getElementById('portuguesa').checked){
+        somaPortuguesa = somaPortuguesa - 1;
+        vendaPortuguesa = vendaPortuguesa - 25.00;
+        if(somaPortuguesa < 0){
+            somaPortuguesa = 0;
+            vendaPortuguesa = 0.00;
+        }
+        document.getElementById('resultadoPortuguesa').innerHTML = somaPortuguesa;
+        document.getElementById('vendaPortuguesa').innerHTML = vendaPortuguesa.toFixed(2);
+    }else if(document.getElementById('calabresa').checked){
+        somaCalabresa = somaCalabresa - 1;
+        vendaCalabresa = vendaCalabresa - 25.00;
+        if(somaCalabresa < 0){
+            somaCalabresa = 0;
+            vendaCalabresa = 0.00;
+        }
+        document.getElementById('resultadoCalabresa').innerHTML = somaCalabresa;
+        document.getElementById('vendaCalabresa').innerHTML = vendaCalabresa.toFixed(2);
+    }
+}
 
 
 

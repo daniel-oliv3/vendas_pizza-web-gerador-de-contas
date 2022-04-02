25e44f6465eba19a7b4a278c44d876dc886231d8
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-04-02 12:56:48 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -24,6 +24,18 @@ function venda(e){
     if(document.getElementById('mussarela').checked){
         somaMussarela = somaMussarela + 1;
         vendaMussarela = vendaMussarela + 25.00;
+        document.getElementById('resultadoMussarela').innerHTML = somaMussarela;
+        document.getElementById('vendaMussarela').innerHTML = vendaMussarela.toFixed(2);
+    }else if(document.getElementById('portuguesa').checked){
+        somaPortuguesa = somaPortuguesa + 1;
+        vendaPortuguesa = vendaPortuguesa + 25.00;
+        document.getElementById('resultadoPortuguesa').innerHTML = somaPortuguesa;
+        document.getElementById('vendaPortuguesa').innerHTML = vendaPortuguesa.toFixed(2);
+    }else if(document.getElementById('calabresa').checked){
+        somaCalabresa = somaCalabresa + 1;
+        vendaCalabresa = vendaCalabresa + 25.00;
+        document.getElementById('resultadoCalabresa').innerHTML = somaCalabresa;
+        document.getElementById('vendaCalabresa').innerHTML = vendaCalabresa.toFixed(2);
     }
 }
 
